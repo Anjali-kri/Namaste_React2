f136b3813802e3c8e5caa923e5bc66a0c50c50a7
Anjali singh <73298495+anjalibobbysingh1@users.noreply.github.com>
2023-05-21 00:26:36 +0530

    01

---
+++ app.js
@@ -0,0 +1,28 @@
+const parent = React.createElement(
+    'div',
+    {id: "parent"},
+    React.createElement(
+        'div',
+        {id: "child"},
+        [React.createElement(
+            'h1',
+            {},
+            "i am h1 tag"
+        ),
+        React.createElement(
+            'h2',
+            {},
+            "i am h1 tag"
+        )]
+    )
+);
+
+// const heading = React.createElement(
+//     "H1",
+//     {id: "heading"}, 
+//     "Hello world from react"
+// );
+// console.log(heading);
+const root = ReactDOM .createRoot(document.getElementById("root"));
+// root.render(heading);
+root.render(parent);
